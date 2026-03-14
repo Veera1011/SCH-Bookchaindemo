db51f9a90b75a4c27df4461f4a5cc33fa4c26ad5
Veera1011 <veera252728@gmail.com>
2026-03-14 17:02:16 +0530

    14-03-2026

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "96996326" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1503077036" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
